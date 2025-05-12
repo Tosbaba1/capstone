201df7dacded2ca2d7dc586e33d1c1d47c289ced
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-12 20:44:22 +0000

    Added home page redirection

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_author", { :controller => "authors", :action => "create" })
-          
+
   # READ
   get("/authors", { :controller => "authors", :action => "index" })
-  
+
   get("/authors/:path_id", { :controller => "authors", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_author/:path_id", { :controller => "authors", :action => "update" })
-  
+
   # DELETE
   get("/delete_author/:path_id", { :controller => "authors", :action => "destroy" })
 
@@ -22,16 +22,18 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_post", { :controller => "posts", :action => "create" })
-          
+
   # READ
+  get("/", { :controller => "posts", :action => "index" })
+
   get("/posts", { :controller => "posts", :action => "index" })
-  
+
   get("/posts/:path_id", { :controller => "posts", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_post/:path_id", { :controller => "posts", :action => "update" })
-  
+
   # DELETE
   get("/delete_post/:path_id", { :controller => "posts", :action => "destroy" })
 
@@ -41,16 +43,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_followrequest", { :controller => "followrequests", :action => "create" })
-          
+
   # READ
   get("/followrequests", { :controller => "followrequests", :action => "index" })
-  
+
   get("/followrequests/:path_id", { :controller => "followrequests", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_followrequest/:path_id", { :controller => "followrequests", :action => "update" })
-  
+
   # DELETE
   get("/delete_followrequest/:path_id", { :controller => "followrequests", :action => "destroy" })
 
@@ -60,16 +62,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_comment", { :controller => "comments", :action => "create" })
-          
+
   # READ
   get("/comments", { :controller => "comments", :action => "index" })
-  
+
   get("/comments/:path_id", { :controller => "comments", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_comment/:path_id", { :controller => "comments", :action => "update" })
-  
+
   # DELETE
   get("/delete_comment/:path_id", { :controller => "comments", :action => "destroy" })
 
@@ -79,16 +81,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_like", { :controller => "likes", :action => "create" })
-          
+
   # READ
   get("/likes", { :controller => "likes", :action => "index" })
-  
+
   get("/likes/:path_id", { :controller => "likes", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_like/:path_id", { :controller => "likes", :action => "update" })
-  
+
   # DELETE
   get("/delete_like/:path_id", { :controller => "likes", :action => "destroy" })
 
@@ -98,16 +100,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_book", { :controller => "books", :action => "create" })
-          
+
   # READ
   get("/books", { :controller => "books", :action => "index" })
-  
+
   get("/books/:path_id", { :controller => "books", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_book/:path_id", { :controller => "books", :action => "update" })
-  
+
   # DELETE
   get("/delete_book/:path_id", { :controller => "books", :action => "destroy" })
 
@@ -118,5 +120,5 @@ Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
-  
+
 end

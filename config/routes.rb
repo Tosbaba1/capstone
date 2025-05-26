3f72b9b77d85b8fe93d79d0779b9fb1f0410affb
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-25 22:02:46 -0500

    Update Ruby version and add persistent login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
   root to: "posts#index"
   get "/library", to: "pages#library"
   get "/notifications", to: "pages#notifications"

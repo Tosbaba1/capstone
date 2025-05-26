0d02a2c27820fa8d955689a86e9815747dba5930
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-25 21:35:17 -0500

    Implement profile page, sample data task, and likes/comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,4 @@
 class CommentsController < ApplicationController
-  def index
-    matching_comments = Comment.all
-
-    @list_of_comments = matching_comments.order({ :created_at => :desc })
-
-    render({ :template => "comments/index" })
-  end
-
-  def show
-    the_id = params.fetch("path_id")
-
-    matching_comments = Comment.where({ :id => the_id })
-
-    @the_comment = matching_comments.at(0)
-
-    render({ :template => "comments/show" })
-  end
 
   def create
     the_comment = Comment.new
@@ -25,25 +8,9 @@ class CommentsController < ApplicationController
 
     if the_comment.valid?
       the_comment.save
-      redirect_to("/comments", { :notice => "Comment created successfully." })
+      redirect_back fallback_location: root_path, notice: "Comment created successfully."
     else
-      redirect_to("/comments", { :alert => the_comment.errors.full_messages.to_sentence })
-    end
-  end
-
-  def update
-    the_id = params.fetch("path_id")
-    the_comment = Comment.where({ :id => the_id }).at(0)
-
-    the_comment.post_id = params.fetch("query_post_id")
-    the_comment.commenter_id = params.fetch("query_commenter_id")
-    the_comment.comment = params.fetch("query_comment")
-
-    if the_comment.valid?
-      the_comment.save
-      redirect_to("/comments/#{the_comment.id}", { :notice => "Comment updated successfully."} )
-    else
-      redirect_to("/comments/#{the_comment.id}", { :alert => the_comment.errors.full_messages.to_sentence })
+      redirect_back fallback_location: root_path, alert: the_comment.errors.full_messages.to_sentence
     end
   end
 
@@ -53,6 +20,6 @@ class CommentsController < ApplicationController
 
     the_comment.destroy
 
-    redirect_to("/comments", { :notice => "Comment deleted successfully."} )
+    redirect_back fallback_location: root_path, notice: "Comment deleted successfully."
   end
 end

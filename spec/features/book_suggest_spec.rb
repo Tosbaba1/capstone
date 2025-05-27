a48375ef8c5ecabecb343fbec32bd5de171add14
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-27 11:46:10 -0500

    Add book suggestions feature

---
+++ spec/features/book_suggest_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Book suggestions", type: :feature, js: true do
+  let(:user) { create(:user) }
+
+  before do
+    login_as(user, scope: :user)
+  end
+
+  it "displays suggestions while typing" do
+    stub_request(:get, "http://www.example.com/books/suggest")
+      .with(query: hash_including(q: "Har"))
+      .to_return(status: 200, body: [{ title: "Harry Potter" }].to_json,
+                 headers: { "Content-Type" => "application/json" })
+
+    visit "/books/external_search"
+
+    fill_in "q", with: "Har"
+
+    expect(page).to have_selector("datalist option[value='Harry Potter']")
+  end
+end

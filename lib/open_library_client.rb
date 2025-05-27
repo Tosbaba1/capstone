c7031c4b3fe2513a2e0f85d4ca22862fe55e9419
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-27 10:36:37 -0500

    Add OpenLibraryClient for external API

---
+++ lib/open_library_client.rb
@@ -0,0 +1,19 @@
+class OpenLibraryClient
+  BASE_SEARCH_URL = 'https://openlibrary.org/search.json'
+  BASE_WORKS_URL  = 'https://openlibrary.org/works'
+  BASE_COVER_URL  = 'https://covers.openlibrary.org/b/id'
+
+  def self.search_books(query)
+    response = HTTP.get(BASE_SEARCH_URL, params: { q: query, key: ENV['book_search_key'] })
+    JSON.parse(response.to_s)
+  end
+
+  def self.fetch_work(work_id)
+    response = HTTP.get("#{BASE_WORKS_URL}/#{work_id}.json", params: { key: ENV['works_key'] })
+    JSON.parse(response.to_s)
+  end
+
+  def self.cover_url(cover_id, size)
+    "#{BASE_COVER_URL}/#{cover_id}-#{size}.jpg?key=#{ENV['covers_key']}"
+  end
+end

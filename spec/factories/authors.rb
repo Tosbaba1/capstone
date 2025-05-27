4341046ab78e39fe9ddb73009cc6edce6fe74043
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-27 15:28:48 +0000

    Migrated in new tables

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id          :bigint           not null, primary key
+#  bio         :string
+#  books_count :integer
+#  dob         :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 FactoryBot.define do
   factory :author do
     name { Faker::Book.author }
--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  genre       :string
+#  image_url   :string
+#  page_length :integer
+#  title       :string
+#  year        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  author_id   :integer
+#  library_id  :integer
+#
 FactoryBot.define do
   factory :book do
     title { Faker::Book.title }

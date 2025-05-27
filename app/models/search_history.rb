ce1f1e4492a928939453b116aed77163be7eb736
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-27 13:45:35 +0000

    Migrated new tables

--- app/models/search_history.rb
+++ app/models/search_history.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: search_histories
+#
+#  id         :bigint           not null, primary key
+#  query      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_search_histories_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class SearchHistory < ApplicationRecord
   belongs_to :user
 

39dc09eaf3fd541e6e322108693d20cf845d3c12
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-27 08:44:26 -0500

    Move follow requests to top and add search history

--- db/schema.rb
+++ db/schema.rb
@@ -118,6 +118,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_02_000000) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "search_histories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "query"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_search_histories_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -138,4 +146,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_02_000000) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "search_histories", "users"
 end

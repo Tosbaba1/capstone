1c1dbb259ee5d1c258b2fa9c2f5bb82c35a79a62
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-28 10:33:22 -0500

    Add book details feature spec

--- spec/features/book_details_spec.rb
+++ spec/features/book_details_spec.rb
@@ -50,4 +50,57 @@ RSpec.describe 'Book details', type: :feature do
     expect(page).to have_content('Another Book')
     expect(page).to have_content('Edition: Another Book - First Edition')
   end
+
+  it 'shows details from search results and allows importing' do
+    search_response = {
+      'docs' => [
+        {
+          'title' => 'Search Book',
+          'author_name' => ['Jane Doe'],
+          'key' => '/works/OL789W',
+          'edition_key' => ['OL2M']
+        }
+      ]
+    }
+
+    stub_request(:get, 'https://openlibrary.org/search.json')
+      .with(query: hash_including(q: 'Search Book'))
+      .to_return(status: 200, body: search_response.to_json,
+                 headers: { 'Content-Type' => 'application/json' })
+
+    work_response = {
+      'title' => 'Search Book',
+      'authors' => [{ 'name' => 'Jane Doe' }],
+      'number_of_pages' => 321
+    }
+
+    edition_response = {
+      'title' => 'Search Book - Second Edition',
+      'number_of_pages' => 321
+    }
+
+    stub_request(:get, 'https://openlibrary.org/works/OL789W.json')
+      .to_return(status: 200, body: work_response.to_json,
+                 headers: { 'Content-Type' => 'application/json' })
+    stub_request(:get, 'https://openlibrary.org/books/OL2M.json')
+      .to_return(status: 200, body: edition_response.to_json,
+                 headers: { 'Content-Type' => 'application/json' })
+
+    visit '/search?tab=books&q=Search+Book'
+    click_link 'Details', match: :first
+
+    expect(page).to have_content('Jane Doe')
+    expect(page).to have_content('Page length: 321')
+    expect(page).to have_content('Edition: Search Book - Second Edition')
+    expect(page).to have_content('Finished: 0')
+    expect(page).to have_content('Reading: 0')
+    expect(page).to have_content('Want to read: 0')
+
+    expect do
+      click_button 'Add to Library'
+    end.to change(Book, :count).by(1).and change(Reading, :count).by(1)
+
+    visit '/books/details/OL789W?edition_id=OL2M'
+    expect(page).to have_content('Want to read: 1')
+  end
 end

7936e927e4f494fc5ee5e2bc5720876604fd2432
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-28 09:30:11 -0500

    Add fetch_edition to OpenLibraryClient

--- lib/open_library_client.rb
+++ lib/open_library_client.rb
@@ -1,6 +1,7 @@
 class OpenLibraryClient
   BASE_SEARCH_URL = 'https://openlibrary.org/search.json'
   BASE_WORKS_URL  = 'https://openlibrary.org/works'
+  BASE_BOOKS_URL  = 'https://openlibrary.org/books'
   BASE_COVER_URL  = 'https://covers.openlibrary.org/b/id'
 
   def self.search_books(query)
@@ -10,6 +11,11 @@ class OpenLibraryClient
 
   def self.fetch_work(work_id)
     response = HTTP.get("#{BASE_WORKS_URL}/#{work_id}.json")
+    JSON.parse(response.to_s)
+  end
+
+  def self.fetch_edition(edition_id)
+    response = HTTP.get("#{BASE_BOOKS_URL}/#{edition_id}.json")
     JSON.parse(response.to_s)
   end
 

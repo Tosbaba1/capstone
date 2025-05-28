81700c80a3dc8da3021396a42c8347190ade4b0d
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-28 15:35:51 +0000

    Migrated new tables

--- spec/factories/followrequests.rb
+++ spec/factories/followrequests.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: followrequests
+#
+#  id           :bigint           not null, primary key
+#  status       :string           default("pending")
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  recipient_id :integer
+#  sender_id    :integer
+#
 FactoryBot.define do
   factory :followrequest do
     association :sender, factory: :user
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id             :bigint           not null, primary key
+#  comments_count :integer
+#  content        :text
+#  likes_count    :integer
+#  poll_data      :jsonb
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  book_id        :integer
+#  creator_id     :integer
+#
 FactoryBot.define do
   factory :post do
     association :creator, factory: :user

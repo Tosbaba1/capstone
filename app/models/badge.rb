8a67c326e59f2a46175dbdef0345ebdf89691748
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-05-30 16:06:57 +0000

    Migrated Tables

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: badges
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  image_url   :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_badges_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Badge < ApplicationRecord
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_19_000000) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_20_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_19_000000) do
     t.integer "books_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "badges", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_badges_on_user_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -151,5 +161,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_19_000000) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "badges", "users"
   add_foreign_key "search_histories", "users"
 end
